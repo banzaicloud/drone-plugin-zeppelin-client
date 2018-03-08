c900d05fb2c59681f756f1c8fe26ee4460c97ca1
Laszlo Puskas <laszlo@banzaicloud.com>
2018-03-08 15:30:17 +0100

    Amended logging, removed unused code

--- plugin.go
+++ plugin.go
@@ -4,56 +4,18 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
-	"github.com/oliveagle/jsonpath"
-	"gopkg.in/go-playground/validator.v9"
 	"io"
 	"io/ioutil"
 	"net/http"
 	"net/http/httputil"
+
+	"github.com/oliveagle/jsonpath"
+	log "github.com/sirupsen/logrus"
+	"gopkg.in/go-playground/validator.v9"
 )
 
 type (
-	Repo struct {
-		Owner   string
-		Name    string
-		Link    string
-		Avatar  string
-		Branch  string
-		Private bool
-		Trusted bool
-	}
-
-	Build struct {
-		Number   int
-		Event    string
-		Status   string
-		Deploy   string
-		Created  int64
-		Started  int64
-		Finished int64
-		Link     string
-	}
-
-	Author struct {
-		Name   string
-		Email  string
-		Avatar string
-	}
-
-	Commit struct {
-		Remote  string
-		Sha     string
-		Ref     string
-		Link    string
-		Branch  string
-		Message string
-		Author  Author
-	}
-
 	Plugin struct {
-		Repo   Repo
-		Build  Build
-		Commit Commit
 		Config Config
 	}
 
@@ -84,12 +46,12 @@ func notebookExists(config *Config) bool {
 func apiCall(url string, method string, username string, password string, body io.Reader) *http.Response {
 	req, err := http.NewRequest(method, url, body)
 
-	if method == "POST" {
+	if method == http.MethodPost {
 		req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
 	}
 
 	if err != nil {
-		Fatalf("failed to build http request: %v", err)
+		log.Fatalf("failed to build http request: %v", err)
 	}
 
 	req.SetBasicAuth(username, password)
@@ -98,13 +60,13 @@ func apiCall(url string, method string, username string, password string, body i
 	resp, err := http.DefaultClient.Do(req)
 
 	if err != nil {
-		Fatalf("failed to call \"%s\" on %s: %+v", method, url, err)
+		log.Fatalf("failed to call \"%s\" on %s: %+v", method, url, err)
 	}
 
 	debugReq, _ := httputil.DumpRequest(req, true)
-	Debugf("Request %s", debugReq)
+	log.Debugf("Request %s", debugReq)
 	debugResp, _ := httputil.DumpResponse(resp, true)
-	Debugf("Response %s", debugResp)
+	log.Debugf("Response %s", debugResp)
 
 	defer resp.Body.Close()
 	return resp
@@ -112,7 +74,7 @@ func apiCall(url string, method string, username string, password string, body i
 
 func lookupNotebookId(config *Config) {
 	url := fmt.Sprintf("%s/api/notebook", config.Endpoint)
-	resp := apiCall(url, "GET", config.Username, config.Password, nil)
+	resp := apiCall(url, http.MethodGet, config.Username, config.Password, nil)
 
 	type (
 		NotebookData struct {
@@ -132,66 +94,66 @@ func lookupNotebookId(config *Config) {
 		err := json.NewDecoder(resp.Body).Decode(&result)
 
 		if err != nil {
-			Fatalf("failed to parse /api/v1/notebook to go struct: %+v", resp)
+			log.Fatalf("failed to parse /api/v1/notebook to go struct: %+v", resp)
 		}
 	}
 
 	for _, notebook := range result.Data {
 		if notebook.Name == config.Notebook.Name {
 			config.Notebook.Id = notebook.Id
-			Infof("Notebook %s found with id: %s", config.Notebook.Name, config.Notebook.Id)
+			log.Infof("Notebook %s found with id: %s", config.Notebook.Name, config.Notebook.Id)
 		}
 	}
 
 	if config.Notebook.Id == "" {
-		Infof("Notebook not found with name: %s", config.Notebook.Name)
+		log.Infof("Notebook not found with name: %s", config.Notebook.Name)
 	}
 }
 
 func deleteNotebook(config *Config) bool {
-	Infof("Deleting notebook ...")
+	log.Infof("deleting notebook ...")
 	url := fmt.Sprintf("%s/api/notebook/%s", config.Endpoint, config.Notebook.Id)
-	resp := apiCall(url, "DELETE", config.Username, config.Password, nil)
+	resp := apiCall(url, http.MethodDelete, config.Username, config.Password, nil)
 
-	if resp.StatusCode == 200 {
-		Infof("Notebook %s (%s) has been deleted", config.Notebook.Name, config.Notebook.Id)
+	if resp.StatusCode == http.StatusOK {
+		log.Infof("Notebook %s (%s) has been deleted", config.Notebook.Name, config.Notebook.Id)
 		config.Notebook.Id = ""
 		return true
 	}
 
-	if resp.StatusCode == 404 {
-		Errorf("Unable to delete notebook %s", config.Notebook.Name)
+	if resp.StatusCode == http.StatusNotFound {
+		log.Errorf("Unable to delete notebook %s", config.Notebook.Name)
 		return false
 	}
 
-	Fatalf("Unexpected error %+v", resp)
+	log.Fatalf("Unexpected error %+v", resp)
 	return false
 }
 
 func importNotebook(config *Config) bool {
 
-	Infof("Importing notebook ...")
+	log.Infof("Importing notebook ...")
 	notebookData, err := ioutil.ReadFile(config.Notebook.FilePath)
 	if err != nil {
 		panic(err)
 	}
 
 	url := fmt.Sprintf("%s/api/notebook/import", config.Endpoint)
-	resp := apiCall(url, "POST", config.Username, config.Password, bytes.NewBuffer(notebookData))
+	resp := apiCall(url, http.MethodPost, config.Username, config.Password, bytes.NewBuffer(notebookData))
 
-	if resp.StatusCode == 200 {
+	if resp.StatusCode == http.StatusOK {
 		//var result = map[string] string{}
 		result := map[string]string{}
 		err = json.NewDecoder(resp.Body).Decode(&result)
 		if err != nil {
-			Fatalf("failed to parse /api/notebook/import to go struct: %+v", err)
+			log.Fatalf("failed to parse /api/notebook/import to go struct: %+v", err)
 		}
 		config.Notebook.Id = result["body"]
-		Infof("Notebook %s has been imported with id: %s", config.Notebook.Name, config.Notebook.Id)
+		log.Infof("Notebook %s has been imported with id: %s", config.Notebook.Name, config.Notebook.Id)
 		return true
 	}
 
-	Fatalf("Unexpected error %+v", resp)
+	log.Fatalf("Unexpected error %+v", resp)
 	return false
 }
 
@@ -201,12 +163,12 @@ func createNotebook(p *Plugin) bool {
 		importNotebook(&p.Config)
 	} else {
 		if notebookInProgress(&p.Config) == false {
-			Infof("Notebook %s (%s) already exists with same name, will be recreated",
+			log.Infof("Notebook %s (%s) already exists with same name, will be recreated",
 				p.Config.Notebook.Name, p.Config.Notebook.Id)
 			deleteNotebook(&p.Config)
 			importNotebook(&p.Config)
 		} else {
-			Infof("Notebook %s (%s) already exists with same name and is in progress",
+			log.Infof("Notebook %s (%s) already exists with same name and is in progress",
 				p.Config.Notebook.Name, p.Config.Notebook.Id)
 			return false
 		}
@@ -217,24 +179,24 @@ func createNotebook(p *Plugin) bool {
 
 func runNotebook(config *Config) bool {
 
-	Infof("Running notebook ...")
+	log.Infof("Running notebook ...")
 	url := fmt.Sprintf("%s/api/notebook/job/%s?waitToFinish=false", config.Endpoint, config.Notebook.Id)
-	resp := apiCall(url, "POST", config.Username, config.Password, nil)
+	resp := apiCall(url, http.MethodPost, config.Username, config.Password, nil)
 
-	if resp.StatusCode == 200 {
-		Infof("Notebook %s (%s) has been started", config.Notebook.Name, config.Notebook.Id)
+	if resp.StatusCode == http.StatusOK {
+		log.Infof("Notebook %s (%s) has been started", config.Notebook.Name, config.Notebook.Id)
 		return true
 	}
-	Fatalf("Unexpected error %+v", resp)
+	log.Fatalf("Unexpected error %+v", resp)
 	return false
 }
 
 func notebookInProgress(config *Config) bool {
 
-	Infof("Checking notebook status ...")
+	log.Infof("Checking notebook status ...")
 	url := fmt.Sprintf("%s/api/notebook/job/%s", config.Endpoint, config.Notebook.Id)
-	resp := apiCall(url, "GET", config.Username, config.Password, nil)
-	if resp.StatusCode != 200 {
+	resp := apiCall(url, http.MethodGet, config.Username, config.Password, nil)
+	if resp.StatusCode != http.StatusOK {
 		return false
 	}
 	data, err := ioutil.ReadAll(resp.Body)
@@ -243,14 +205,14 @@ func notebookInProgress(config *Config) bool {
 
 	res, err := jsonpath.JsonPathLookup(jsonData, "$.body[?(@.progress < 100)].status")
 	if err != nil {
-		Fatalf("failed to lookup path in json: %s", err)
+		log.Fatalf("failed to lookup path in json: %s", err)
 	}
 
 	var inProgress = false
 	if len(res.([]interface{})) > 0 {
 		inProgress = true
 	}
-	Infof("Notebook %s (%s) is in progress: %t", config.Notebook.Name, config.Notebook.Id, inProgress)
+	log.Infof("Notebook %s (%s) is in progress: %t", config.Notebook.Name, config.Notebook.Id, inProgress)
 	return inProgress
 }
 
@@ -260,12 +222,12 @@ func (p *Plugin) Exec() error {
 	err := validate.Struct(p)
 	if err != nil {
 		for _, v := range err.(validator.ValidationErrors) {
-			Errorf("[%s] field validation error (%+v)", v.Field(), v)
+			log.Errorf("[%s] field validation error (%+v)", v.Field(), v)
 		}
 		return nil
 	}
 
-	Infof("Notebook desired state: %s", p.Config.Notebook.State)
+	log.Infof("Notebook desired state: %s", p.Config.Notebook.State)
 	lookupNotebookId(&p.Config)
 
 	switch p.Config.Notebook.State {
@@ -280,7 +242,7 @@ func (p *Plugin) Exec() error {
 		if notebookExists(&p.Config) == true {
 			deleteNotebook(&p.Config)
 		} else {
-			Infof("Notebook %s doesn't exists, nothing to do ", p.Config.Notebook.Name)
+			log.Infof("Notebook [%s] doesn't exists, nothing to do ", p.Config.Notebook.Name)
 		}
 	}
 
